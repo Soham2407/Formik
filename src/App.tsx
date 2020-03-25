92aac8a2a40576dfea83cb3bc081a657d2c406fb
Soham2407 <nesarikarsoham24@gmail.com>
2020-03-25 10:37:32 +0530

    formik and yup demo created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FormikDemo from './FormikDemo';
+
+function App() {
+  return (
+    <div className="App">
+     <FormikDemo/>>
+    </div>
+  );
+}
+
+export default App;
